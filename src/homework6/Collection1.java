fb74480bf0b7b0159d0c8cd1d925389c47b0c0f4
gabynico <gaby_nico@hotmail.com>
2016-02-07 12:14:30 +0200

    partial commit

--- src/homework6/Collection1.java
+++ src/homework6/Collection1.java
@@ -3,7 +3,7 @@ package homework6;
 /**
  * Created by ns on 1/30/2016.
  */
-public interface Collection1<T> {
+public abstract interface Collection1<T> {
     public boolean containsAll(T c);
     public boolean addAll(T c);
 }
---
+++ src/homework6/Collection1ExtendInterface.java
@@ -0,0 +1,9 @@
+package homework6;
+
+/**
+ * Created by ns on 2/7/2016.
+ */
+public interface Collection1ExtendInterface<T> extends Collection1<T> {
+
+
+}
